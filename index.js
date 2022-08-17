3c2b39dcf83a140d78489b7eab458f507f57f9f2
Elizabeth Mutesi <elizabethmutesi@gmail.com>
2022-08-17 19:52:06 +0300

    added mvc

--- index.js
+++ index.js
@@ -1,9 +1,57 @@
 const express = require("express")
 const path = require("path")
-
-
-
+const mongoose = require("mongoose")
+const workerRoutes = require("./routes/workerRoutes")
 const app = express()
+
+
+app.set("views", path.join(__dirname, "/views"))
+app.set("view engine", "html")
+
+
+app.use(express.urlencoded({ extended: true }));
+
+
+//db connect
+//mongodb://localhost:27017
+mongoose.connect("mongodb://localhost:27017/farm",{
+    useNewUrlParser: true,
+    useUnifiedTopology: true},
+    (err) => {
+        if(!err) console.log("Connected to mongoDB");
+        else console.log("Error connecting to mongoDB " + err)
+    
+    })
+
+
+
+
+    app.use("/", workerRoutes)
+
+
+
+
+//create a schema
+    // const workerSchema = new mongoose.Schema({
+    //     name: String,
+    //     field: String,
+    //     age: Number,
+    //     salary: Number,
+    // })
+    //create a model
+    // const workerModel = new mongoose.model("Worker", workerSchema)
+
+    // const data = new workerModel({
+    //     name: "Duchess",
+    //     field: "Coffee",
+    //     age: 30,
+    //     salary: 50000,
+    // })
+    // data.save()
+
+
+
+    
 
 app.get("/", (req, res)=> {
     res.sendFile(path.join(__dirname, "/index.html"))
@@ -14,35 +62,34 @@ app.get("/file", (req, res)=> {
 })
 
 
-
 // create a route (get)
-app.get("/just", (req, res)=> {   // the / is a route, req (request),res (response) is a call back - an urgument of another function
-    res.send("This is my homepage")
-})
+// app.get("/just", (req, res)=> {   // the / is a route, req (request),res (response) is a call back - an urgument of another function
+//     res.send("This is my homepage")
+// })
 
-// send something to the front end (post)
-// app.post
-// delete (delete)
-//uupdate the current information (update/ boot)
+// // send something to the front end (post)
+// // app.post
+// // delete (delete)
+// //uupdate the current information (update/ boot)
 
 
-app.get("/home", (req, res)=> {
-    res.send("Welcome to our homepage")
-})
+// app.get("/home", (req, res)=> {
+//     res.send("Welcome to our homepage")
+// })
 
-app.get("/profile", (req, res)=> {
-    res.send("Welcome to our Profile")
-})
+// app.get("/profile", (req, res)=> {
+//     res.send("Welcome to our Profile")
+// })
 
-app.get("/gallery", (req, res)=> {
-    res.send("Welcome to our gallery")
-})
+// app.get("/gallery", (req, res)=> {
+//     res.send("Welcome to our gallery")
+// })
 
-//CRAD
-// Create,- User inputs information into the application and it persists
-// Read, - Get information from the system and read it
-// Authenticate - Able to update/ authenticate the data
-//and Delete - Forego
+// //CRAD
+// // Create,- User inputs information into the application and it persists
+// // Read, - Get information from the system and read it
+// // Authenticate - Able to update/ authenticate the data
+// //and Delete - Forego
 
 
 
